7941b4f37f59fd2c9662de3155b50cb56cdcf091
ldoublewood <ldoublewood@gmail.com>
2019-12-25 11:38:23 +0800

    add registry script

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+# registry proxy
+
+this script aims to run a docker registry cache proxy.
+
--- scripts/run_registry.sh
+++ scripts/run_registry.sh
@@ -1,6 +1,7 @@
-docker run -d --rm --name registry -p 5000:5000 -v registry:2
+docker run -d --rm --name registry -p 5000:5000 registry:2
 docker cp registry:/etc/docker/registry/config.yml config.yml
-echo "proxy: \
-  remoteurl: https://registry.docker-cn.com \
+docker stop registry
+echo "proxy:\n\
+  remoteurl: https://registry.docker-cn.com\n\
   #remoteurl: http://f1374db2.m.daocloud.io" >> config.yml
 docker run -d --name registry -p 5000:5000 -v registry:/var/lib/registry -v ${PWD}/config.yml:/etc/docker/registry/config.yml registry:2
