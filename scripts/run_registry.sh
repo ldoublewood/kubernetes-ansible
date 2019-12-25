927a3061b4f9d2b3762febe9e4dbf43905482f33
ldoublewood <ldoublewood@gmail.com>
2019-12-25 12:04:53 +0800

    add registry

--- scripts/run_registry.sh
+++ scripts/run_registry.sh
@@ -1,7 +1,6 @@
 docker run -d --rm --name registry -p 5000:5000 registry:2
 docker cp registry:/etc/docker/registry/config.yml config.yml
 docker stop registry
-echo "proxy:\n\
-  remoteurl: https://registry.docker-cn.com\n\
-  #remoteurl: http://f1374db2.m.daocloud.io" >> config.yml
+echo -e "proxy:\n\
+  remoteurl: http://hub-mirror.c.163.com" >> config.yml
 docker run -d --name registry -p 5000:5000 -v registry:/var/lib/registry -v ${PWD}/config.yml:/etc/docker/registry/config.yml registry:2
